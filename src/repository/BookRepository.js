fa5d4cfce0cd151d738623596316fbc8751890bf
silver_kir <rci@mp03.ru>
2025-04-08 22:27:43 +0800

    mongoose fixed

--- src/repository/BookRepository.js
+++ src/repository/BookRepository.js
@@ -1,39 +1,36 @@
 const bookSchema = require("../model/bookSchema");
 
-let instanse=null;
+let instanse = null;
 
 module.exports = class BookRepository {
-  
-    constructor() {
-        if(instanse){
-            return instanse;
-        }
-        instanse=this;
-        this.store = bookSchema
-        return instanse
+  constructor() {
+    if (instanse) {
+      return instanse;
     }
+    instanse = this;
+    this.store = bookSchema;
+    return instanse;
+  }
 
-     async getAll() {
-         return await this.store.find().select('-__v');
-     }
+  async getAll() {
+    return await this.store.find().select("-__v");
+  }
 
+  async getBookById(id) {
+    return await this.store.findOne({ id: id }).select("-__v");
+  }
 
-    async getBookById(id) {
-        return await this.store.findById(id).select('-__v');
-    }
+  async addBook(book) {
+    const newBook = new this.store(book);
+    await newBook.save();
+    return newBook;
+  }
 
-    async addBook(book) {
-        const newBook = new this.store(book);
-        await newBook.save();
-        return newBook;
-    }
+  async updateBook(id, book) {
+    await this.store.findOneAndUpdate({ id: id }, book).select("-__v");
+  }
 
-    async updateBook(id, book) {
-      await this.store.findByIdAndUpdate(id, book).select('-__v');
-    }
-    
-    async deleteBook(id) {
-        await this.store.findByIdAndDelete(id).select('-__v');
-    }
-
-}
+  async deleteBook(id) {
+    await this.store.findOneAndDelete({ id: id }).select("-__v");
+  }
+};
